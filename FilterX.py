adb36c04557ca756376c27bde134e70d8636e0ab
K4sh1r1nkut0 <56045160+YanOScompany@users.noreply.github.com>
2021-04-18 10:08:58 -0400

    Add files via upload

---
+++ FilterX.py
@@ -0,0 +1,40 @@
+import os 
+import shutil
+from settings import types
+from colorama import Fore
+from banner import banner
+
+print(Fore.GREEN + banner)
+
+os.system("clear")
+
+def main():
+    files = os.listdir()
+
+    for file in files:
+        for key, value in types.items():
+            for i in range(len(value)):
+                if file.endswith(value[i]):
+                    if file == "FilterX.py" or file == "settings.py" or file == "banner.py":
+                        continue
+
+                    if value[i] in types[key]:
+                        if not os.path.isdir(key):
+                            q = input(Fore.RED + "Do you want to create new?(Y/N): ")
+                            if q == "Y":
+                                os.mkdir(key)
+                                curdir = os.path.join(os.getcwd(), file)
+                                new_dir = os.path.join(os.getcwd(), key, file)
+                                shutil.move(curdir, new_dir)
+                                print("\n" + Fore.YELLOW + "[~]File | " + Fore.GREEN + file + Fore.YELLOW + " | has been moved from | " + Fore.GREEN + curdir + Fore.YELLOW + " | to | " + Fore.GREEN + new_dir)
+                            else:
+                                print("[~]Then byee!")
+                                exit()
+                        else:
+                            curdir = os.path.join(os.getcwd(), file)
+                            new_dir = os.path.join(os.getcwd(), key, file)
+                            shutil.move(curdir, new_dir)
+                            print("\n" + Fore.YELLOW + "[~]File | " + Fore.GREEN + file + Fore.YELLOW + " | has been moved from | " + Fore.GREEN + curdir + Fore.YELLOW + " | to | " + Fore.GREEN + new_dir)
+
+if __name__ == "__main__":
+    main()
---
+++ settings.py
@@ -0,0 +1,6 @@
+types = {
+    "Audio & Video" : ['.mp3', 'mp4', '.avi', 'wav', 'wma'],
+    "Images": ['.png', 'jpg', '.gif', '.jpeg'],
+    "Scripts" : [".py", ".cs", ".cpp", ".c",".sh",".bash",".hrc", ".js",".rb"],
+    "Books" : [".pdf", ".PDF"]    
+}
